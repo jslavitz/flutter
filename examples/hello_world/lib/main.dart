219b1a05427b77450ba131b074f2a688251a4cb6
jslavitz <jslavitz@google.com>
2019-01-28 12:50:14 -0800

    example

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -3,5 +3,56 @@
 // found in the LICENSE file.
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+//void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+void main() {
+
+  runApp(CupertinoPickerDemo());
+
+}
+class CupertinoPickerDemo extends StatefulWidget {
+  @override
+  _CupertinoPickerDemoState createState() => _CupertinoPickerDemoState();
+}
+
+class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            CupertinoButton(
+              child: const Text('Reset Picker'),
+              onPressed: () {
+                CupertinoDatePicker.reset(context);
+              },
+            ),
+            Container(
+                height: 300,
+                child:
+                CupertinoPicker(
+                  itemExtent: 20.0,
+                  onSelectedItemChanged: (int v) {
+
+                  },
+                  children: const <Widget>[
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                    Text('1'),
+                  ],
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
