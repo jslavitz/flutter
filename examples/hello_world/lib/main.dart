f12523f47d0cdb19f6c00e290826afd269935fc9
jslavitz <jslavitz@google.com>
2019-02-05 14:39:50 -0800

    example

--- examples/hello_world/lib/main.dart
+++ examples/hello_world/lib/main.dart
@@ -3,5 +3,119 @@
 // found in the LICENSE file.
 
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/cupertino.dart';
+//void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
 
-void main() => runApp(const Center(child: Text('Hello, world!', textDirection: TextDirection.ltr)));
+void main() {
+  runApp(TestWidget());
+}
+
+
+class TestWidget extends StatefulWidget {
+
+  TestWidget();
+
+  @override
+  TestWidgetState createState() => TestWidgetState();
+}
+
+class TestWidgetState extends State<TestWidget> {
+
+  double radius = 85.0;
+  double width = 200.0;
+  double height = 200.0;
+
+  final double max = 200.0;
+
+  RoundedRectCornerMode mode = RoundedRectCornerMode.dynamicShape;
+  bool isRoundedCornerMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(40.0) + const EdgeInsets.only(top: 30.0),
+              height: 300,
+              alignment: Alignment.center,
+              child: Material(
+                elevation: 20.0,
+                color: Colors.lightBlue,
+                shape: CupertinoRoundedRectangleBorder(
+                  borderRadius: radius,
+                  mode: mode,
+                ),
+                child: Container(
+                  width: width,
+                  height: height,
+                ),
+              ),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text('Radius: ' + radius.toString()),
+                  CupertinoSlider(
+                    min: 0,
+                    max: 85,
+                    value: radius,
+                    onChanged: (double value) {
+                       setState(() {
+                         radius = value;
+                       });
+                    }
+                  ),
+                  Text('Width: ' + width.toString()),
+                  CupertinoSlider(
+                    max: max,
+                    min: 0,
+                    value: width,
+                    onChanged: (double value) {
+                      setState(() {
+                        width = value;
+                      });
+                    }
+                  ),
+                  Text('Height: ' + height.toString()),
+                  CupertinoSlider(
+                    max: max,
+                    min: 0,
+                    value: height,
+                    onChanged: (double value) {
+                      setState(() {
+                        height = value;
+                      });
+                    }
+                  ),
+                  Container(
+                    height: 120,
+                    child: CupertinoPicker(
+                      backgroundColor: Colors.white12,
+                      itemExtent: 30,
+                      children: const <Widget>[
+                        Text('Dynamic Shape'),
+                        Text('Dynamic Radius'),
+                      ],
+                      onSelectedItemChanged: (int item) {
+                        setState(() {
+                          print('hjere');
+                          mode = item == 0 ? RoundedRectCornerMode.dynamicShape :
+                            RoundedRectCornerMode.dynamicRadius;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
